edbd010c650d4323c4cc38c656be480518c68f67
Lucas Passos Vanderlinde <lucaspvanderlinde@hotmail.com>
2020-10-17 10:09:46 -0300

    Projeto gerado com Spring Initializr

---
+++ back-end/study/src/test/java/com/br/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
